ec4aedad416bc1e9b0db214ee2175377e49c90de
DingYF <1617681854@qq>
2023-07-02 20:59:45 +0800

    常规修改

--- Debug/Debug.ino
+++ Debug/Debug.ino
@@ -1,119 +1,101 @@
 #include <Servo.h>
 
-int input1 = 3; // 定义uno的pin 5 向 input1 输出
-int input2 = 11; // 定义uno的pin 6 向 input2 输出
-int input3 = 5; // 定义uno的pin 9 向 input3 输出
-int input4 = 6; // 定义uno的pin 10 向 input4 输出
+int analog1 = 3; // 定义uno的pin 5 向 analog1 输出
+int analog2 = 11; // 定义uno的pin 6 向 analog2 输出
+int analog3 = 5; // 定义uno的pin 9 向 analog3 输出
+int analog4 = 6; // 定义uno的pin 10 向 analog4 输出
+int trigPin = 13;                // 超声波模块的Trig引脚
+int echoPin = 12;                // 超声波模块的Echo引脚
+
 // int servoPin = 11;   // 舵机引脚
 Servo myServo;    // 创建一个 Servo 对象
 int angle = 0;    // 初始角度
-bool avodiMode = false;
+bool avoidMode = true;         // 是否启用避障模式
 
 void setup() {
   Serial.begin(9600);
-  pinMode(input1,OUTPUT);
-  pinMode(input2,OUTPUT);
-  pinMode(input3,OUTPUT);
-  pinMode(input4,OUTPUT);
+  pinMode(analog1,OUTPUT);
+  pinMode(analog2,OUTPUT);
+  pinMode(analog3,OUTPUT);
+  pinMode(analog4,OUTPUT);
   myServo.attach(9);    // 将 Servo 对象与数字引脚 9 关联
+  pinMode(trigPin, OUTPUT);    // 配置超声波模块的Trig引脚为输出模式
+  pinMode(echoPin, INPUT);     // 配置超声波模块的Echo引脚为输入模式
   // pinMode(servoPin,OUTPUT);
 }
 
 void loop() {
-    // back();
-    // delay(1000);
-    // stop();
-    // delay(1000);
-    // forward();
-    // delay(1000);
-
-  if(avodiMode)
-    {
-      for (angle = 0; angle <= 180; angle += 1)
-        {  // 逐渐增加角度
-          myServo.write(angle);    // 设置舵机位置
-          delay(15);               // 等待一段时间
-        }
-      for (angle = 180; angle >= 0; angle -= 1) 
-      {  // 逐渐减小角度
-      myServo.write(angle);    // 设置舵机位置
-      delay(15);               // 等待一段时间
-    }
-      avodiMode = false;
-    }
-  else{
-    back();
-    delay(1000);
-    stop();
-    delay(1000);
-    forward();
-    delay(1000);
-    stop();
-    delay(1000);
-    avodiMode = true;
-  }
+  rightward();
+  delay(375);
+  stop();
+  delay(3000);
 }
 
 void forward() {
-  analogWrite(input1,55);
-  analogWrite(input2,0);
-  analogWrite(input3,55);
-  analogWrite(input4,0);
+  analogWrite(analog1,0);
+  analogWrite(analog2,55);
+  analogWrite(analog3,0);
+  analogWrite(analog4,55);
 }
-void back() {
-  analogWrite(input1,0);
-  analogWrite(input2,55);
-  analogWrite(input3,0);
-  analogWrite(input4,55);
+void backward() {
+  analogWrite(analog1,55);
+  analogWrite(analog2,0);
+  analogWrite(analog3,55);
+  analogWrite(analog4,0);
+  
 }
+void leftward() {
+  analogWrite(analog1,0);
+  analogWrite(analog2,55);
+  analogWrite(analog3,55);
+  analogWrite(analog4,0);
+}
+void rightward() {
+  analogWrite(analog1,55);
+  analogWrite(analog2,0);
+  analogWrite(analog3,0);
+  analogWrite(analog4,55);
+}
+
 void stop() {
-  analogWrite(input1,0);
-  analogWrite(input2,0);
-  analogWrite(input3,0);
-  analogWrite(input4,0);
+  analogWrite(analog1,0);
+  analogWrite(analog2,0);
+  analogWrite(analog3,0);
+  analogWrite(analog4,0);
 }
 
-///////////////////////////////////////////////////////////////////////////////////
+int detectObstacle() {
+  long duration, distance;
+  digitalWrite(trigPin, LOW);     // 准备发射超声波信号
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);    // 发射超声波信号
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);  // 计算超声波信号往返时间
+  distance = duration * 0.034 / 2;
+  return distance;         // 计算距离（单位：厘米）
+}
 
-// int input1 = 3;  // 模拟输出引脚
-// int input2 = 9;  // 模拟输出引脚
-// int input3 = 5;  // 模拟输出引脚
-// int input4 = 6;  // 模拟输出引脚
-
-// void setup() {
-//   // 将引脚模式设置为输出
-//   pinMode(input1, OUTPUT);
-//   pinMode(input2, OUTPUT);
-//   pinMode(input3, OUTPUT);
-//   pinMode(input4, OUTPUT);
-// }
-
-// void loop() {
-//   back();
-//   delay(1000);
-//   stop();
-//   delay(1000);
-//   forward();
-//   delay(1000);
-// }
-
-// void forward() {
-//   analogWrite(input1,0);
-//   analogWrite(input2,55); 
-//   analogWrite(input3,0);
-//   analogWrite(input4,55); 
-// }
-// void back() {
-//   analogWrite(input1,55);
-//   analogWrite(input2,0);
-//   analogWrite(input3,55);
-//   analogWrite(input4,0); 
-// }
-// void stop() {
-//   analogWrite(input1,0);
-//   analogWrite(input2,0);
-//   analogWrite(input3,0);
-//   analogWrite(input4,0); 
-// }
-
+void steer(int& value1, int& value2) {
+  for (angle = 90; angle <= 180; angle += 1){  // 逐渐增加角度
+  myServo.write(angle);    // 设置舵机位置
+  delay(15);               // 等待一段时间
+  if (angle == 180){
+  value1 = detectObstacle();
+  Serial.print("distance");
+  Serial.println(value1);
+  }
+  }
+  myServo.write(90); 
+  delay(1000);
+  for (angle = 90; angle >= 0; angle -= 1) {  // 逐渐减小角度
+  myServo.write(angle);    // 设置舵机位置
+  delay(15);             // 等待一段时间
+  if (angle == 0){
+  value2 = detectObstacle();
+  }
+  }
+  myServo.write(90); 
+  delay(1000);
+}
 
